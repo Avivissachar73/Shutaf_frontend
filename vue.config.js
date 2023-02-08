d7f444b95e914b05e3d5606961eeaaa25f0cebfb
Aviv Issachar <avivi@misterbit.co.il>
2023-02-08 18:18:30 +0200

    manifest and app config

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,22 @@
-const CompressionPlugin = require('compression-webpack-plugin');
+// const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   outputDir: '../backend/public/frontend',
+  pages: {
+    index: {
+      entry: "src/main.js",
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: "Shutaf"
+    }
+  },
   configureWebpack: {
     plugins: [
-      new CompressionPlugin({
-        test: /\.js$|\.css$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.8
-      })
+      // new CompressionPlugin({
+      //   test: /\.js$|\.css$|\.html$/,
+      //   threshold: 10240,
+      //   minRatio: 0.8
+      // })
     ]
   }
 }
