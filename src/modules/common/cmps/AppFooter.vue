6b14101651fa1083baec2b964c633c043e49b892
Aviv Issachar <avivi@misterbit.co.il>
2023-02-24 00:16:55 +0200

    footer update

--- src/modules/common/cmps/AppFooter.vue
+++ src/modules/common/cmps/AppFooter.vue
@@ -1,8 +1,10 @@
 <template>
   <footer class="app-footer flex-center">
-    <div class="container">
+    <h4 class="flex align-center gap5 wrap space-between">
       <CreditLogo/>
-    </div>
+      © Aviv Issachar
+      <a style="color: #feffbd;" :href="cvLink" target="_blank"><h3>- C.V -</h3></a>
+    </h4>
   </footer>
 </template>
 
@@ -10,7 +12,12 @@
 import CreditLogo from './CreditLogo.vue'
 export default {
   name: "AppFooter",
-  components: { CreditLogo }
+  components: { CreditLogo },
+  data() {
+    return {
+      cvLink: 'https://docs.google.com/document/d/1chtvUmRJo38ZakzSWxqIyDgI6K1xi0eS/edit?usp=sharing&ouid=103323829961679615098&rtpof=true&sd=true'
+    }
+  }
 }
 </script>
 
